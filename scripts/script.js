7f06aa95c0f9a4b390842c5b4c634061b49d81a3
professorguanabara <66076428+professorguanabara@users.noreply.github.com>
2025-07-23 12:56:49 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,7 @@ botao.addEventListener('click', () => {
 });
 
 // Scroll suave para links de navegação
-const navLinks = document.querySelectorAll('#menu ul a');
+const navLinks = document.querySelectorAll('#menu ul a.link');
 navLinks.forEach(link => {
   link.addEventListener('click', function(e) {
     e.preventDefault();
